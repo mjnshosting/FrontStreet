57a7b23ebda7d5e3e1d6e49db160aeacae34574b
Young Grizzly <support@mjnshosting.com>
2018-10-16 23:56:33 -0700

    finishing Roku script

--- setup/roku_tv_setup.sh
+++ setup/roku_tv_setup.sh
@@ -16,7 +16,7 @@ I='Info'
 BS='Backspace'
 SH='Search'
 E='Enter'
- 
+
 echo "Change script where necessary in case menu items and order change"
 echo "This should be used only on TVs that have been newly setup with no changes made."
 echo "You can 10secs to cancel before execution"
@@ -28,8 +28,8 @@ sleep 1
 echo "...1"
 sleep 1
 echo "...0"
- 
-#Disable Screen Saver
+
+echo "Disable Screen Saver"
 curl -d '' $TV$H
 sleep .5
 curl -d '' $TV$U
@@ -48,8 +48,8 @@ curl -d '' $TV$S
 sleep .5
 curl -d '' $TV$S
 sleep .5
- 
-#Disable Speakers
+
+echo "Disable Speakers"
 curl -d '' $TV$H
 sleep .5
 curl -d '' $TV$U
@@ -76,38 +76,8 @@ curl -d '' $TV$S
 sleep .5
 curl -d '' $TV$S
 sleep .5
- 
-#Disable Auto Power Savings
-curl -d '' $TV$H
-sleep .5
-curl -d '' $TV$U
-sleep .5
-curl -d '' $TV$S
-sleep .5
-curl -d '' $TV$U
-sleep .5
-curl -d '' $TV$S
-sleep .5
-curl -d '' $TV$D
-sleep .5
-curl -d '' $TV$D
-sleep .5
-curl -d '' $TV$D
-sleep .5
-curl -d '' $TV$S
-sleep .5
-curl -d '' $TV$D
-sleep .5
-curl -d '' $TV$S
-sleep .5
-curl -d '' $TV$S
-sleep .5
-curl -d '' $TV$D
-sleep .5
-curl -d '' $TV$S
-sleep .5
- 
-#Disable Standby LED
+
+echo "Disable Auto Power Savings"
 curl -d '' $TV$H
 sleep .5
 curl -d '' $TV$U
@@ -128,14 +98,16 @@ curl -d '' $TV$S
 sleep .5
 curl -d '' $TV$D
 sleep .5
+curl -d '' $TV$S
+sleep .5
+curl -d '' $TV$S
+sleep .5
 curl -d '' $TV$D
 sleep .5
 curl -d '' $TV$S
 sleep .5
-curl -d '' $TV$S
-sleep .5
- 
-#Disable Fast TV Start
+
+echo "Disable Standby LED"
 curl -d '' $TV$H
 sleep .5
 curl -d '' $TV$U
@@ -158,14 +130,42 @@ curl -d '' $TV$D
 sleep .5
 curl -d '' $TV$D
 sleep .5
+curl -d '' $TV$S
+sleep .5
+curl -d '' $TV$S
+sleep .5
+
+echo "Disable Fast TV Start"
+curl -d '' $TV$H
+sleep .5
+curl -d '' $TV$U
+sleep .5
+curl -d '' $TV$S
+sleep .5
+curl -d '' $TV$U
+sleep .5
+curl -d '' $TV$S
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$S
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$D
+sleep .5
 curl -d '' $TV$D
 sleep .5
 curl -d '' $TV$S
 sleep .5
 curl -d '' $TV$S
 sleep .5
- 
-#Disable USB Media Auto Launch
+
+echo "Disable USB Media Auto Launch"
 curl -d '' $TV$H
 sleep .5
 curl -d '' $TV$U
@@ -192,6 +192,6 @@ curl -d '' $TV$U
 sleep .5
 curl -d '' $TV$S
 sleep .5
- 
-#Return Home
+
+echo "Return Home"
 curl -d '' $TV$H
