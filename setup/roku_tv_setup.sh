63e6a6e2759b1de6cdc3ae1683cb3eaef0dd29fa
Young Grizzly <support@mjnshosting.com>
2018-10-16 17:16:00 -0700

    moving things around

---
+++ setup/roku_tv_setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+TV='http://192.168.1.184:8060/keypress/'
+H='Home'
+R='Rev'
+F='Fwd'
+P='Play'
+S='Select'
+LT='Left'
+RT='Right'
+D='Down'
+U='Up'
+B='Back'
+IR='InstantReplay'
+I='Info'
+BS='Backspace'
+SH='Search'
+E='Enter'
+
+echo "Change script where necessary in case menu items and order change"
+echo "This should be used only on TVs that have been newly setup with no changes made."
+echo "You can 10secs to cancel before execution"
+sleep 7
+echo "Beginning in 3"
+sleep 1
+echo "...2"
+sleep 1
+echo "...1"
+sleep 1
+echo "...0"
+
+#Disable Screen Saver
+curl -d '' $TV$H
+sleep .5
+curl -d '' $TV$U
+sleep .5
+curl -d '' $TV$S
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$S
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$S
+sleep .5
+curl -d '' $TV$S
+sleep .5
+
+#Disable Speakers
+curl -d '' $TV$H
+sleep .5
+curl -d '' $TV$U
+sleep .5
+curl -d '' $TV$S
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$D
+sleep .5
+curl -d '' $TV$S
+sleep .5
+curl -d '' $TV$S
+sleep .5
+curl -d '' $TV$S
+sleep .5
+curl -d '' $TV$S
+sleep .5
+
+
